380501cee8e9f7a9c501f6ea8aa2edfc64da0b15
marc0601 <marckhalil06@gmail.com>
2018-05-05 12:14:11 +0200

    Revert "cpufreq: add microvolt unit for undervolting for use with other tweakers"
    This reverts commit 04f2698b85639e91dc6c0fef0236d0fa7f6c2cbc.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -558,9 +558,6 @@ static ssize_t show_scaling_setspeed(struct cpufreq_policy *policy, char *buf)
 extern ssize_t show_UV_mV_table(struct cpufreq_policy *policy, char *buf);
 extern ssize_t store_UV_mV_table(struct cpufreq_policy *policy,
                                       const char *buf, size_t count);
-extern ssize_t store_UV_uV_table(struct cpufreq_policy *policy,
-				 const char *buf, size_t count);
-extern ssize_t show_UV_uV_table(struct cpufreq_policy *policy, char *buf);
 
 /* sysfs interface for ASV level */
 extern ssize_t show_asv_level(struct cpufreq_policy *policy, char *buf);
@@ -598,7 +595,6 @@ cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
 /* UV table */
 cpufreq_freq_attr_rw(UV_mV_table);
-cpufreq_freq_attr_rw(UV_uV_table);
 /* ASV level */
 cpufreq_freq_attr_rw(asv_level);
 
@@ -615,7 +611,6 @@ static struct attribute *default_attrs[] = {
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
 	&UV_mV_table.attr,
-	&UV_uV_table.attr,
 	&asv_level.attr,
 	NULL
 };
